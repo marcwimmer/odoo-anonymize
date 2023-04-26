13c38c6c8ce4f1a9fe659f8818653ef3613bf144
Marc Wimmer <marc@itewimmer.de>
2023-04-26 13:18:48 +0200

    from 14.0

--- anonymize/models/fields.py
+++ anonymize/models/fields.py
@@ -1,45 +1,128 @@
 from odoo.addons.queue_job.job import job
+import random
 from odoo import _, api, fields, models, SUPERUSER_ID
 from odoo.exceptions import UserError, RedirectWarning, ValidationError
+from .cities import city_names
+
 
 class Fields(models.Model):
-    _inherit = 'ir.model.fields'
+    _inherit = "ir.model.fields"
+    _domains = [
+        "hotmail.com",
+        "gmail.com",
+        "aol.com",
+        "mail.com",
+        "mail.kz",
+        "yahoo.com",
+    ]
 
-    anonymize = fields.Boolean("Anonymize")
+    anonymize = fields.Selection(
+        [
+            ["clear", "Clear"],
+            ["fullname", "Name"],
+            ["lastname", "Last-Name"],
+            ["firstname", "First-Name"],
+            ["street", "Street"],
+            ["phone", "Phone"],
+            ["city", "City"],
+        ],
+        "Anonymize",
+    )
+    anonymize_length = fields.Integer("Anonymize Length")
+
+    @api.model
+    def gen_phone(self):
+        first = str(random.randint(00000, 99999))
+        second = str(random.randint(10000, 99999)).zfill(7)
+
+        last = str(random.randint(1, 99)).zfill(2)
+        while last in ["1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888"]:
+            last = str(random.randint(1, 9998)).zfill(4)
+
+        return "{}/{}-{}".format(first, second, last)
 
     @api.constrains("anonymize")
     def _check_anonymize_flag(self):
         for self in self:
-            if self.ttype not in ['char', 'text']:
+            if self.ttype not in ["char", "text"]:
                 raise ValidationError("Only chars can be anonymized!")
 
     @api.model
-    def _get_excluded_anonymize_models(self):
-        return [
-            'res.config.settings',
-        ]
+    def _apply_default_anonymize_fields(self):
+        if self.search_count([("anonymize", "!=", False)]):
+            return
+
+        for dbfield in self.env["ir.model.fields"].search(
+            [("ttype", "in", ["char", "text"])]
+        ):
+            for x in [
+                ("phone", "phone"),
+                ("lastname", "lastname"),
+                ("firstname", "firstname"),
+                ("city", "city"),
+                ("zip", "number", 5),
+                ("fax", "phone"),
+                ("email", "email"),
+            ]:
+                self.env.cr.execute(
+                    "update ir_model_fields set anonymize = %s where id = %s",
+                    (
+                        x[1],
+                        dbfield.id,
+                    ),
+                )
+                if len(x) > 2:
+                    self.env.cr.execute(
+                        "update ir_model_fields set anonymize_length = %s where id = %s",
+                        (
+                            x[2],
+                            dbfield.id,
+                        ),
+                    )
+
+        for dbfield in self.env["ir.model.fields"].search(
+            [("model", "=", "res.partner"), ("name", "in", ["display_name", "name"])]
+        ):
+            self.env.cr.execute(
+                "update ir_model_fields set anonymize = 'fullname'  where id = %s",
+                (dbfield.id,),
+            )
 
     @api.model
-    def _apply_default_anonymize_fields(self):
-        name_fields = {}
-        for dbfield in self.env['ir.model.fields'].search([
-            ('ttype', 'in', ['char', 'text']),
-            ('model_id.model', 'not in', self._get_excluded_anonymize_models())
-            ]):
-            if any(x in dbfield.name for x in [
-                'phone',
-                'lastname',
-                'firstname',
-                'city',
-                'zip',
-                'fax',
-                'email',
-            ]):
-                self.env.cr.execute("update ir_model_fields set anonymize = true where id = %s", (dbfield.id,))
+    def get_one_random_domain(self, domains):
+        return random.choice(domains)
 
-        for dbfield in self.env['ir.model.fields'].search([
-            ('model', '=', 'res.partner'),
-            ('name', 'in', ['display_name', 'name'])
-        ]):
-            self.env.cr.execute("update ir_model_fields set anonymize = true where id = %s", (dbfield.id,))
-            # dbfield.anonymize = True does not work, says change through python
+    @api.model
+    def generate_random_email(self):
+        import names
+
+        return (
+            names.get_full_name().replace(" ", ".")
+            + "@"
+            + self.get_one_random_domain(self._domains)
+        )
+
+    def _anonymize_value(self, val):
+        import names
+
+        if self.anonymize == "fullname":
+            return names.get_full_name()
+        elif self.anonymize == "lastname":
+            return names.get_last_name()
+        elif self.anonymize == "email":
+            return self.generate_random_email()
+        elif self.anonymize == "phone":
+            return self.gen_phone()
+        elif self.anonymize == "city":
+            return random.choice(Fields.city_names)
+        elif self.anonymize == "clear":
+            if self.ttype in ["char", "text"]:
+                return ""
+            elif self.ttype in ["date", "datetime"]:
+                return None
+            elif self.ttype in ["int", "float"]:
+                return 0
+            else:
+                raise NotImplementedError(self.type)
+        else:
+            raise NotImplementedError(self.anonymize)
