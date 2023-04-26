bbd779575af461dbce265b4d8f59e2931124291c
Marc Wimmer <marc@itewimmer.de>
2023-04-26 12:49:02 +0200

    taken from 14.0

--- anonymize/models/__init__.py
+++ anonymize/models/__init__.py
@@ -1,3 +1,3 @@
-import os
-import glob
-__all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
+from . import cities
+from . import fields
+from . import anonymizer
